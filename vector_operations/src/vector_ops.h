48b52ca0223303ef6ecd232901f35e2e89ef763d
Savkina Ekaterina <mykolesiko@mail.ru>
2020-10-06 01:27:23 +0300

    first change

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -1,11 +1,133 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
+using namespace std;
 
 namespace task {
 
 // Your code here...
+    const double EPS_double = 1e-7;
 
+    vector<double> operator+ (const vector<double>& a, const vector<double>& b){
+        vector<double> c;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            c.push_back(a[i] + b[i]);
+        }
+        return (c);
+    }
+    vector<double> operator- (const vector<double>& a, const vector<double>& b) {
+        vector<double> c;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            c.push_back(a[i] - b[i]);
+        }
+        return (c);
+    }
+
+
+    vector<double> operator+ (const vector<double>& a) {
+        vector<double> c;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            c.push_back(a[i]);
+        }
+        return (c);
+    }
+    vector<double> operator- (const vector<double>& a) {
+        vector<double> c;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            c.push_back(-a[i]);
+        }
+        return (c);
+    }
+
+
+    double operator* (const vector<double>& a, const vector<double>& b) {
+        double dp = 0;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            dp += (a[i] * b[i]);
+        }
+        return (dp);
+    }
+
+    vector<double> operator% (const vector<double>& a, const vector<double>& b) {
+        vector<double> c;
+        c.push_back(a[2] * b[1] - a[1] * b[2]);
+        c.push_back(a[0] * b[2] - a[2] * b[0]);
+        c.push_back(a[1] * b[0] - a[0] * b[1]);
+        return (c);
+    }
+    bool operator|| (const vector<double>& a, const vector<double>& b) {
+        double dp = a * b;
+        double len1 = 0;
+        double len2 = 0;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            len1 += a[i] * a[i];
+            len2 += b[i] * b[i];
+
+        }
+        len1 = sqrt(len1);
+        len2 = sqrt(len2);
+        double prop = fabs(dp/(len1 * len2));
+        if (fabs(prop - 1) < EPS_double) return true;
+        return (false);
+    }
+    bool operator&& (const vector<double>& a, const vector<double>& b) {
+        if ((a || b) == false) return false;
+        if (a * b > 0) return true;
+        else return false;
+    }
+    std::iostream& operator<< (std::iostream &out, const vector<double> a) {
+        for (int i = 0; i < a.size(); ++i)
+        {
+            if (i == a.size() - 1)
+                out << a[i] << endl;
+            else
+                out << a[i] << " ";
+        }
+        return out;
+    }
+
+    std::iostream& operator>> (std::iostream &in, vector<double>& a) {
+        int vec_size;
+        in >> vec_size;
+        a.clear();
+        for (int i = 0; i < vec_size; ++i)
+        {
+            double temp;
+            in >> temp;
+            a.push_back(temp);
+        }
+        return in;
+    }
+
+    void reverse(vector<double>& a) {
+        std::reverse( a.begin(), a.end() );
+    }
+
+
+    vector<int> operator| (const vector<int>& a, const vector<int>& b) {
+        vector<int> c;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            c.push_back(a[i] | b[i]);
+        }
+        return (c);
+    }
+
+    vector<int> operator& (const vector<int>& a, const vector<int>& b) {
+        vector<int> c;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            c.push_back(a[i] & b[i]);
+        }
+        return (c);
+    }
 
 }  // namespace task
