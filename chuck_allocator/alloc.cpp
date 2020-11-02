f2afffb47af0f62b6b0e7eefcd4e46c831cc0cbb
Savkina Ekaterina <mykolesiko@mail.ru>
2020-11-02 18:06:11 +0300

    fixed review bugs

--- chuck_allocator/alloc.cpp
+++ chuck_allocator/alloc.cpp
@@ -5,62 +5,68 @@
 #include <cstdint>
 #include <memory>
 #include <iostream>
+#include <new>
 
 using namespace std;
 
 class Chunk {
 public:
-    Chunk(const std::size_t size) : next(nullptr), ptr_begin((uint8_t*) this), ptr_tek((uint8_t*) this) {}
+    Chunk(const std::size_t size);
     Chunk* next;
-    uint8_t* ptr_tek;
-    uint8_t* ptr_begin;
+    uint8_t* ptr_tek; //текущий указатель на свободное место
+    uint8_t* ptr_begin; //указатель на начало чанка
 };
+
+Chunk :: Chunk(const std::size_t size) {
+    next = nullptr;
+    ptr_begin = static_cast<uint8_t*>(operator new (size));
+    ptr_tek = ptr_begin;
+}
+
 
 class Chunks_storage {
  public:
-    Chunks_storage(): first(nullptr), end(nullptr), n_chunks(0) {}
+    Chunks_storage(): first(nullptr), end(nullptr), n_chunks(0), ref_counts(1) {}
     ~Chunks_storage();
  private:
     Chunk* first;
     Chunk* end;
     int n_chunks;
-    const int chank_size = 100;
+    const int chunk_size = 10000;
 
  public:
-    static int ref_counts;
+    int ref_counts;
     uint8_t* allocate(const std::size_t size);
     Chunk* add_new_chunk();
-
 };
 
 Chunk* Chunks_storage :: add_new_chunk() {
     if (!first) {
-        end = first = new Chunk(chank_size);
+        end = first = new Chunk(chunk_size);
         n_chunks = 1;
     } else {
         Chunk* end_old = end;
-        end = new Chunk(chank_size);
+        end = new Chunk(chunk_size);
         end_old->next = end;
         n_chunks ++;
     }
     return(end);
 }
 
-int Chunks_storage :: ref_counts = 1;
-
 uint8_t* Chunks_storage :: allocate(const std::size_t size) {
-
-    if (size > chank_size) {
-        //!!!
+    if (size > chunk_size) {
+        throw(std::bad_alloc());
     }
     Chunk* chunk_cur = first;
     if (first == nullptr)
        chunk_cur =  add_new_chunk();
     while (chunk_cur != nullptr) {
-        if ((chank_size - (chunk_cur->ptr_tek - chunk_cur->ptr_begin)) > size) {
+        if ((chunk_size - (chunk_cur->ptr_tek - chunk_cur->ptr_begin)) > size) {
             uint8_t* ptr_tek_old = chunk_cur->ptr_tek;
             chunk_cur->ptr_tek += size;
             return ptr_tek_old;
+        } else {
+           chunk_cur =  chunk_cur->next;
         }
     }
     chunk_cur = add_new_chunk();
@@ -80,90 +86,62 @@ class allocator_my {
     template< class U >
     struct rebind {
         using other =  allocator_my<U>;
+        //typedef allocator_my<U> other;
     };
-
     Chunks_storage* chunks;
-
-    //rebind (deprecated in C++17)(removed in C++20)	template< class U > struct rebind { typedef allocator<U> other; };
-
-
-
-
  public:
     //creates a new allocator instance
     //(public member function)
     allocator_my() : chunks(new Chunks_storage()) {}
-    allocator_my(allocator_my<T>& copy);
+    allocator_my(const allocator_my<T>& copy);
     ~allocator_my();
-    allocator_my<T>& operator=(allocator_my<T> copy);
-
-
-
+    allocator_my<T>& operator=(const allocator_my<T>& copy);
     //(public member function)
     //allocates uninitialized storage
     T* allocate(const size_type n );
-
-
     //(public member function)
     //deallocates storage
-    void deallocate(T* p, size_t size);
-
-    template < class T, class... Args>
+    void deallocate(T* p, size_t size) {};
+    template <class... Args>
        void construct(T *p, Args &&...args) {
            new ((void *) p) T(std::forward<Args>(args)...);
        }
-
     //destructs an object in allocated storage
     //(public member function)
     void destroy(T* ptr);
-
-
-
 };
-
-
-
 
 template< class T > T* allocator_my<T> :: allocate(const size_type size) {
     return reinterpret_cast<T*>(chunks->allocate(size * sizeof(value_type)));
 };
 
-
-
-template< class T > void allocator_my<T> :: deallocate(T* p, size_t size) {
-
-}
-
 template< class T > void allocator_my<T> :: destroy(T* ptr) {
     ptr->~T();
 }
 
-
-
-
-template< class T > allocator_my<T> :: allocator_my(allocator_my<T>& copy) {
+template< class T > allocator_my<T> :: allocator_my(const allocator_my<T>& copy) {
     chunks = copy.chunks;
     chunks->ref_counts++;
-
 }
 
-template< class T > allocator_my<T>& allocator_my<T> :: operator=(allocator_my<T> copy) {
+template< class T > allocator_my<T>& allocator_my<T> :: operator=(const allocator_my<T>& copy) {
     if (chunks->ref_counts <= 1) {
+        chunks->ref_counts--;
         delete chunks;
+    } else {
+        chunks->ref_counts--;
     }
-    chunks->ref_counts--;
     chunks = copy.chunks;
     chunks->ref_counts++;
+    return (*this);
 }
-
 
 template< class T > allocator_my<T> :: ~allocator_my() {
-    if (chunks->ref_counts <= 1) {
+    chunks->ref_counts--;
+    if (chunks->ref_counts == 0) {
         delete chunks;
     }
-    chunks->ref_counts--;
 }
-
 
 Chunks_storage :: ~Chunks_storage() {
     Chunk* chunk_cur = first;
@@ -172,8 +150,6 @@ Chunks_storage :: ~Chunks_storage() {
         chunk_cur = chunk_cur_old->next;
         delete(chunk_cur_old);
     }
-
 }
-
 
 #endif // ALLOC_H
